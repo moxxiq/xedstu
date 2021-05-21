70b58ea627893373715c9a88b1f74abc8914110e
Moxxiq <dimanavsisto@gmail.com>
2021-05-21 14:12:51 +0300

    Edit example

---
+++ SRC/hello.py
@@ -0,0 +1,15 @@
+if 2 + 2 != 1 + 1:
+    print("hello, світе 👋")
+
+while 1 * 1 == 2 / 2:
+    pass
+
+s_eng = 'some text in english'
+
+s_eng_d = "some text in english with douvble quotes"
+
+s_ukr = 'якийсь текст'
+
+s_ukr_d = "якийсь текст в подвійних лапках"
+
+# якийсь коментар
